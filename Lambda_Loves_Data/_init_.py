7429ecaa48704247028374d493b80fd2f81c776e
Zhenya Warshavsky <zwarshavsky@gmail.com>
2019-10-28 13:24:27 -0400

    commit w utility files

---
+++ Lambda_Loves_Data/_init_.py
@@ -0,0 +1,14 @@
+'''
+lambdata - a collection of data science helper functions
+'''
+
+import pandas as pd
+import numpy as np
+
+# sample code
+ONES = pd.DataFrame(np.ones(10))
+ZEROS = pd.DataFrame(np.zeros(50))
+
+# sample functions
+def increment(x):
+    pass
